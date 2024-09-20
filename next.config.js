afb34876622e887f9373327260e410c4deb033a8
Lakshmi Sindhe <SESA615581@se.com>
2024-09-20 18:38:25 +0530

    seventcommit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Ensure no custom output directory is set
     distDir: 'build', // Default output directory for Next.js builds
-}
+  };
+  module.exports = nextConfig;
